440ae6f548f9ab52fb7319432c4f058f590c2607
010binary <augustine32fg@gmail.com>
2026-08-31 12:14:44 +0100

    Refactor Counter sketch to improve readability and structure; add detailed digitalWrite sequences

--- Counter/Counter.ino
+++ Counter/Counter.ino
@@ -1,7 +1,101 @@
+int one = 26;
+int two = 25;
+int four = 33;
+int eight = 32;
+
+long delayStop = 750;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(one, OUTPUT);
+  pinMode(two, OUTPUT);
+  pinMode(four, OUTPUT);
+  pinMode(eight, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  // confirmation
+  digitalWrite(one, HIGH);
+  digitalWrite(two, HIGH);
+  digitalWrite(four, HIGH);
+  digitalWrite(eight, HIGH);
+  delay(delayStop);
+
+  // 1
+  digitalWrite(one, HIGH);
+  digitalWrite(two, LOW);
+  digitalWrite(four, LOW);
+  digitalWrite(eight, LOW);
+  delay(delayStop);
+
+  // 2
+  digitalWrite(one, LOW);
+  digitalWrite(two, HIGH);
+  digitalWrite(four, LOW);
+  digitalWrite(eight, LOW);
+  delay(delayStop);
+
+  // 3
+  digitalWrite(one, HIGH);
+  digitalWrite(two, HIGH);
+  digitalWrite(four, LOW);
+  digitalWrite(eight, LOW);
+  delay(delayStop);
+
+  // 4
+  digitalWrite(one, LOW);
+  digitalWrite(two, LOW);
+  digitalWrite(four, HIGH);
+  digitalWrite(eight, LOW);
+  delay(delayStop);
+
+  // 5
+  digitalWrite(one, HIGH);
+  digitalWrite(two, LOW);
+  digitalWrite(four, HIGH);
+  digitalWrite(eight, LOW);
+  delay(delayStop);
+
+  // 6
+  digitalWrite(one, LOW);
+  digitalWrite(two, HIGH);
+  digitalWrite(four, HIGH);
+  digitalWrite(eight, LOW);
+  delay(delayStop);
+
+  // 7
+  digitalWrite(one, HIGH);
+  digitalWrite(two, HIGH);
+  digitalWrite(four, HIGH);
+  digitalWrite(eight, LOW);
+  delay(delayStop);
+
+  // 8
+  digitalWrite(one, LOW);
+  digitalWrite(two, LOW);
+  digitalWrite(four, LOW);
+  digitalWrite(eight, HIGH);
+  delay(delayStop);
+
+  // 9
+  digitalWrite(one, HIGH);
+  digitalWrite(two, LOW);
+  digitalWrite(four, LOW);
+  digitalWrite(eight, HIGH);
+  delay(delayStop);
+
+  // 10
+  digitalWrite(one, LOW);
+  digitalWrite(two, HIGH);
+  digitalWrite(four, LOW);
+  digitalWrite(eight, HIGH);
+  delay(delayStop);
+  delay(delayStop);
+
+  // confirmation
+  digitalWrite(one, HIGH);
+  digitalWrite(two, HIGH);
+  digitalWrite(four, HIGH);
+  digitalWrite(eight, HIGH);
+  delay(delayStop);
 }
