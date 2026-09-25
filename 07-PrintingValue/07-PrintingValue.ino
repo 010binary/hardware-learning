9001cd37041355164eb4727f3d38f961a584f522
010binary <augustine32fg@gmail.com>
2026-09-25 14:35:09 +0100

    Added Printing Value

--- 07-PrintingValue/07-PrintingValue.ino
+++ 07-PrintingValue/07-PrintingValue.ino
@@ -1,9 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
+int n = 255;
+float v = 1.72643;
+int delayRate = 500;
 
-}
+void setup() { Serial.begin(115200); }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println("--- print vs println ---");
+  Serial.print("a");
+  Serial.print("b");
+  Serial.println("c");
 
+  delay(delayRate);
+
+  Serial.println("--- float precision ---");
+  Serial.println(v);
+  Serial.println(v, 4);
+
+  delay(delayRate);
+
+  Serial.println("--- number bases ---");
+  Serial.println(n, DEC);
+  Serial.println(n, HEX);
+  Serial.println(n, BIN);
+
+  delay(delayRate);
+
+  Serial.println("--- printf ---");
+  Serial.printf("n=%d  v=%.3f\n", n, v);
+
+  delay(delayRate);
 }
